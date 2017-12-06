deb7f2517ab615683d2be2541b54308853094e0f
kevin-england <kevinengland@kevins-MacBook-Air.local>
2017-12-06 12:03:27 -0800

    #1. Importing routing module, new components and all new routes added for components to the app.component.ts file

--- my-app/src/app/app.module.ts
+++ my-app/src/app/app.module.ts
@@ -2,24 +2,60 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {NoopAnimationsModule} from '@angular/platform-browser/animations';
-
-
+import {MatButtonModule, MatCheckboxModule} from '@angular/material';
+import { PortfolioComponent } from './portfolio/portfolio.component';
+import { RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
+import { AboutComponent } from './about/about.component';
+import { HomeComponent } from './home/home.component';
+import { ContactComponent } from './contact/contact.component';
+import { ClientsComponent } from './clients/clients.component';
 
+const ROUTES = [
+    {
+      path: '',
+      redirectTo: 'home',
+      pathMatch: 'full'
+    },
+    {
+      path: 'home',
+      component: HomeComponent
+    },
+    {
+      path: 'contact',
+      component: ContactComponent
+    },
+    {
+      path: 'clients',
+      component: ClientsComponent
+    },
+    {
+      path: 'portfolio',
+      component: PortfolioComponent
+    },
+    {
+      path: 'about',
+      component: AboutComponent
+    }
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PortfolioComponent,
+    AboutComponent,
+    HomeComponent,
+    ContactComponent,
+    ClientsComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule, 
+    RouterModule.forRoot(ROUTES)
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
-
-import {MatButtonModule, MatCheckboxModule} from '@angular/material';
 
 @NgModule({
   imports: [MatButtonModule, MatCheckboxModule],
