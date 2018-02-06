87951a888fd20c8e9eee557a8a79779a3d8bc478
Kevin England <kevinengland5051@gmail.com>
2018-02-06 12:59:26 -0800

    #8. Added the JavaScript necessary to get the Materialize ScrollFire component working as a new feature for all of the different pages.

--- my-app/src/js/index.js
+++ my-app/src/js/index.js
@@ -33,5 +33,22 @@ $(document).ready(function() {
           bottom: $target.offset().top + $target.outerHeight() - $this.height()
         });
     });
+
+    /* Materialize ScrollFire Component*/
+    var options = [
+      {selector: '#staggered-test', offset: 50, callback: function(el) {
+        Materialize.toast("This is our ScrollFire Demo!", 1500 );
+      } },
+      {selector: '#staggered-test', offset: 205, callback: function(el) {
+        Materialize.toast("Please continue scrolling!", 1500 );
+      } },
+      {selector: '#staggered-test', offset: 400, callback: function(el) {
+        Materialize.showStaggeredList($(el));
+      } },
+      {selector: '#image-test', offset: 500, callback: function(el) {
+        Materialize.fadeInImage($(el));
+      } }
+    ];
+    Materialize.scrollFire(options);
         
 });
