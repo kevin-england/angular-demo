4d2eae3b77cca63806bb6342a35a4fde9806c952
Kevin England <kevinengland5051@gmail.com>
2018-04-11 13:26:32 -0700

    Update

--- server.js
+++ server.js
@@ -23,12 +23,11 @@ app.get('*', (req, res) => {
  */
 const port = process.env.PORT || '3000';
 
-const hostname = '127.0.0.1';
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
   res.end('Hello World\n');
 });
-server.listen(port, hostname, () => {
+server.listen(port, () => {
   console.log(`Server running at http://${hostname}:${port}/`);
 });
