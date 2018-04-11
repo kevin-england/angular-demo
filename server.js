405e2cd5b5c3b60252e2abee287c9e773d34f341
Kevin England <kevinengland5051@gmail.com>
2018-04-11 13:09:41 -0700

    #11. Server update.

--- server.js
+++ server.js
@@ -28,3 +28,13 @@ app.set('port', port);
  * Listen on provided port, on all network interfaces.
  */
 app.listen(port, () => console.log(`API running on localhost:${port}`));
+
+const hostname = '127.0.0.1';
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('Hello World\n');
+});
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
