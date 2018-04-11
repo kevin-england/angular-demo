a846606842dcf6fc076f1053d461f4bdf9e5399c
Kevin England <kevinengland5051@gmail.com>
2018-04-11 13:33:19 -0700

    Update

--- server.js
+++ server.js
@@ -21,7 +21,6 @@ app.get('*', (req, res) => {
 /**
  * Get port from environment and store in Express.
  */
-const http = require('http');
 const hostname = '127.0.0.1';
 const port = 3000;
 const server = http.createServer((req, res) => {
