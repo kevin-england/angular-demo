7f9e91fd699fbd0b7f30f4400b90c9b7e10bf4c1
Kevin England <kevinengland5051@gmail.com>
2018-04-11 13:30:57 -0700

    Update

--- server.js
+++ server.js
@@ -21,13 +21,14 @@ app.get('*', (req, res) => {
 /**
  * Get port from environment and store in Express.
  */
-const port = process.env.PORT || '3000';
-
+const http = require('http');
+const hostname = '127.0.0.1';
+const port = 3000;
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
   res.end('Hello World\n');
 });
-server.listen(port, () => {
+server.listen(port, hostname, () => {
   console.log(`Server running at http://${hostname}:${port}/`);
 });
