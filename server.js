c2fa3112596579d32881ddc1eee2784c05b606ff
Kevin England <kevinengland5051@gmail.com>
2018-04-11 13:55:41 -0700

    Update

--- server.js
+++ server.js
@@ -21,5 +21,5 @@ app.get('*', (req, res) => {
 /**
  * Get port from environment and store in Express.
  */
-var port_number = server.listen(process.env.PORT || 3000);
-app.listen(port_number);
+let port = process.env.PORT || 8080;
+app.listen(port);
