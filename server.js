3d6db69e4f971b758191518545fe251d31eda453
Kevin England <kevinengland5051@gmail.com>
2018-04-11 13:41:16 -0700

    Update

--- server.js
+++ server.js
@@ -21,13 +21,10 @@ app.get('*', (req, res) => {
 /**
  * Get port from environment and store in Express.
  */
-const hostname = '127.0.0.1';
-const port = 3000;
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World\n');
-});
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
-});
+const port = process.env.PORT || '3000';
+app.set('port', port);
+
+/**
+ * Listen on provided port, on all network interfaces.
+ */
+app.listen(port, () => console.log(`API running on localhost:${port}`));
